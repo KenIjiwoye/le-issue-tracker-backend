2722f7b599f629c09d61ed1b0e2bb1a8a92f6b17
KenIjiwoye <alistluxury@gmail.com>
2021-08-01 14:04:32 +0000

    backend configured

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a63bff2d2955473fa2295ef2e542e99a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bbc595e5-aa41-40d4-94a7-2f3b143d8ebe'
+};
